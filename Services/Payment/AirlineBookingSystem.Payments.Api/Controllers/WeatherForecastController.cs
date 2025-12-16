fca2126860857108fe283273db9b375293d8b20f
Bassent Mahmoud <Bassent.Mahmoud@linkdev.com>
2025-12-16 12:06:49 +0200

    intiating projects structure fot the services

---
+++ Services/Payment/AirlineBookingSystem.Payments.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirlineBookingSystem.Payments.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<string> Get()
+    {
+        return Summaries;
+    }
+}
