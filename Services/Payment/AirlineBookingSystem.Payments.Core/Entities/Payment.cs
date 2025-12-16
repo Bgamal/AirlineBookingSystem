e77232a93ff9a48078794455a0323d54b45b912e
Bassent Mahmoud <Bassent.Mahmoud@linkdev.com>
2025-12-16 12:47:38 +0200

    adding the enitites in all services

---
+++ Services/Payment/AirlineBookingSystem.Payments.Core/Entities/Payment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirlineBookingSystem.Payments.Core.Entities
+{
+    public class Payment
+    {
+        public Guid Id { get; set; }
+        public Guid BookingId { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime PaymentDate { get; set; }
+
+    }
+}
